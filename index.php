4e4d6f9a0b85df7193e2da8ed0d59919e38b839d
Jessica Luyet <jluyet@gmail.com>
2020-10-08 16:46:37 +0200

    Correction du port MySQL (80 to 3306)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/');
+define('BASE_URL', '/comem-archidep-php-todo-exercise');
 // AVEC TEST ICI POUR VOIR SI BIEN SYNCHRONISE
 
 // Database connection parameters.
@@ -13,7 +13,7 @@ define('DB_USER', 'todolist');
 define('DB_PASS', 'Th!sISaPassword!');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '80');
+define('DB_PORT', '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
